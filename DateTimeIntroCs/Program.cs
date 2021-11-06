94045e7cb61b1892382fa5b0130dc19762c6f6cf
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-06 15:36:37 -0500

    added current time

--- DateTimeIntroCs/Program.cs
+++ DateTimeIntroCs/Program.cs
@@ -6,7 +6,12 @@ namespace DateTimeIntroCs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime dateTime = new DateTime(2021, 11, 6);
+            DateTime now = DateTime.Now;
+            DateTime today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Time: " + now.Minute);
         }
     }
 }
